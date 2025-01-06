bbb24ce857e58856fab39bc70b5eb7f52e03c2b5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-06 21:04:34 +0500

    Middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { signIn, signUp } from "../controllers/users.controllers.js";
+import { authenticateUser } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 
-router.post("/signup", signUp)
-router.post("/login", signIn)
+router.post("/signup", signUp);
+router.post("/login", signIn);
+router.get("/verifyUser", authenticateUser, (req, res) => {
+    res.json({ message: "Hey! You Are Logged In", user: req.user });
+});
 
 export default router;
