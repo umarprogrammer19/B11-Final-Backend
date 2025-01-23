988ffa186c0cec5cb41641769babe5c1c4196b79
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-24 01:44:40 +0500

    Checkout Route Setted

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import connectdb from "./src/db/index.js";
 import authRouter from "./src/routes/auth.routes.js";
 import productRouter from "./src/routes/products.routes.js";
 import orderRouter from "./src/routes/orders.routes.js";
+import checkoutRouter from "./src/routes/checkout.routes.js"
 import swaggerUi from "swagger-ui-express";
 import swaggerDocs from "./swaggerConfig.js";
 import homePageUI from "./src/pages/homePage.js";
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use("/api/v1", authRouter);
 app.use("/api/v2", productRouter);
 app.use("/api/v3", orderRouter);
+app.use("/api/v4", checkoutRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req, res) => {
