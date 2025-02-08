3114326ed1db888c9ee328388b9ebf878bfe2819
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-08 20:54:05 +0500

    Delete Api Done

--- src/admin/handlers/user.handlers.js
+++ src/admin/handlers/user.handlers.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import usersModels from "../../models/users.models.js";
 
 export const getAllUsers = async (req, res) => {
@@ -22,5 +23,23 @@ export const getAllUsers = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ message: "Id is Missing" });
+    if (!req.user) return res.status(400).json({ message: "Please Login First" });
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({ message: "Invalid Id" });
+
+    try {
+        const isUser = usersModels.findById(id);
+        if (!isUser) res.status(404).json({ message: "User Not Found" });
+
+        const user = await usersModels.findByIdAndDelete(id);
+        res.status(200).json({ message: "Product deleted successfully", user });
+    } catch (error) {
+        console.error("Error occurred:", error);
+        res.status(500).json({ message: "An error occurred" });
+    }
 }
 
--- src/admin/routes/user.routes.js
+++ src/admin/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllUsers } from "../handlers/user.handlers.js";
+import { deleteUser, getAllUsers } from "../handlers/user.handlers.js";
 import { authenticateAdmin } from "../middleware/admin.middleware.js";
 const router = express.Router();
 
 router.get("/users", authenticateAdmin, getAllUsers);
+router.delete("/users/delete/:id", authenticateAdmin, deleteUser);
 
 export default router;
