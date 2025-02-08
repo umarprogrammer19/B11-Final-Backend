396988f4b18930b0d70bea45a1c52e39be0fcf04
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-08 21:02:56 +0500

    Update Api

--- src/admin/handlers/user.handlers.js
+++ src/admin/handlers/user.handlers.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import usersModels from "../../models/users.models.js";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = async (req, res) => {
     if (!req.user) return res.status(400).json({ message: "Please Login First" });
@@ -43,3 +44,44 @@ export const deleteUser = async (req, res) => {
     }
 }
 
+export const editUser = async (req, res) => {
+    const { id } = req.params;
+    const { fullname, email, password } = req.body;
+
+    if (!fullname && !email && !password) {
+        return res.status(400).json({ message: "Please Enter Atleast One Field" });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: "Invalid ID" });
+    }
+
+    if (!req.user) {
+        return res.status(401).json({ message: "Please Login First" });
+    }
+
+    try {
+        const user = await usersModels.findById(id);
+
+        if (!user) {
+            return res.status(404).json({ message: "User Not Found" });
+        }
+
+        let updatedPassword = password;
+        if (password) {
+            updatedPassword = await bcrypt.hash(password, 10);
+        }
+
+        const updatedUser = await usersModels.findByIdAndUpdate(id, {
+            fullname: fullname || user.fullname,
+            email: email || user.email,
+            password: updatedPassword || user.password
+        }, { new: true });
+
+        return res.status(200).json({ message: "User updated successfully", user: updatedUser });
+
+    } catch (error) {
+        console.error("Error occurred:", error);
+        return res.status(500).json({ message: "An error occurred while updating the user", error: error.message });
+    }
+};
