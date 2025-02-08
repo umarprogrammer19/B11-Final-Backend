1766e125f8fd2e1cc72f0eca85d7893b72ca1efa
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-08 22:21:47 +0500

    Get All Orders

--- src/admin/routes/orders.routes.js
+++ src/admin/routes/orders.routes.js
@@ -3,6 +3,6 @@ import { getAllOrders } from "../handlers/orders.handlers.js";
 import { authenticateAdmin } from "../middleware/admin.middleware.js";
 const router = express.Router();
 
-router.post("/orders", authenticateAdmin, getAllOrders);
+router.get("/orders", authenticateAdmin, getAllOrders);
 
 export default router;
