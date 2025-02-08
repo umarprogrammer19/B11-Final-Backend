5e24e320f3f29b3dd81f5f14ebadd98b2174368f
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-08 21:06:00 +0500

    Successfully Updated The User By Calling An Api

--- src/admin/routes/user.routes.js
+++ src/admin/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { deleteUser, getAllUsers } from "../handlers/user.handlers.js";
+import { deleteUser, editUser, getAllUsers } from "../handlers/user.handlers.js";
 import { authenticateAdmin } from "../middleware/admin.middleware.js";
 const router = express.Router();
 
 router.get("/users", authenticateAdmin, getAllUsers);
 router.delete("/users/delete/:id", authenticateAdmin, deleteUser);
+router.patch("/users/update/:id", authenticateAdmin, editUser);
 
 export default router;
