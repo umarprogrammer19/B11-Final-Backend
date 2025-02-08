5ef49bc55ca0325fa8cba60bc6b3b322f13f2bd5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-08 23:24:41 +0500

    Orders Done

--- src/admin/handlers/orders.handlers.js
+++ src/admin/handlers/orders.handlers.js
@@ -2,12 +2,12 @@ import furniroModels from "../../models/furniro.models.js";
 
 export const getAllOrders = async (req, res) => {
     try {
-        const page = parseInt(req.query.page, 10) || 1;
-        const limit = parseInt(req.query.limit, 10) || 10;
+        const page = parseInt(req.query.page, 15) || 1;
+        const limit = parseInt(req.query.limit, 15) || 15;
         const skip = (page - 1) * limit;
         const orders = await furniroModels.find()
             .skip(skip)
-            .limit(limit);
+            .limit(limit).populate("user");
 
         const totalOrders = await furniroModels.countDocuments();
         res.status(200).json({
