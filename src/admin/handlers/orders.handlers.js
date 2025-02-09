93f7eeed75bcb5fc5acd5782c1d5d7d7b47f01b8
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-09 21:24:33 +0500

    Updation

--- src/admin/handlers/orders.handlers.js
+++ src/admin/handlers/orders.handlers.js
@@ -2,8 +2,8 @@ import furniroModels from "../../models/furniro.models.js";
 
 export const getAllOrders = async (req, res) => {
     try {
-        const page = parseInt(req.query.page, 15) || 1;
-        const limit = parseInt(req.query.limit, 15) || 15;
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 10;
         const skip = (page - 1) * limit;
         const orders = await furniroModels.find()
             .skip(skip)
