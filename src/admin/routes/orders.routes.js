f056ffda308aa94a041856be3a90f9eaf19f5e0b
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-02-11 00:02:24 +0500

    SUccessfully Update Order Status

--- src/admin/routes/orders.routes.js
+++ src/admin/routes/orders.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllOrders } from "../handlers/orders.handlers.js";
+import { getAllOrders, updateStatus } from "../handlers/orders.handlers.js";
 import { authenticateAdmin } from "../middleware/admin.middleware.js";
 const router = express.Router();
 
 router.get("/orders", authenticateAdmin, getAllOrders);
+router.patch("/orders/updateStatus/:id", authenticateAdmin, updateStatus);
 
 export default router;
